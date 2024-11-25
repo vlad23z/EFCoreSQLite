da35659def4103c4c693b8ef30688192ed428a77
vlad23z <zinchenko.vlad.96@mail.ru>
2024-11-25 21:08:51 +0300

    Added for loop

--- ConsoleApp25,11,2024/Program.cs
+++ ConsoleApp25,11,2024/Program.cs
@@ -3,5 +3,9 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello!");
+        for(int i=1;i<=6;i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
